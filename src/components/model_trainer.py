39ad7a796de4b74f9ba3a6e33973e3036e8d0409
Amir Deghatipour <a.deghatipour@gmail.com>
2025-04-28 11:57:28 +0330

    hyper-parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -36,8 +36,60 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeClassifier(class_weight='balanced'),
                 "Random Forest Classifier": RandomForestClassifier(class_weight='balanced'),
                 "K-Neighbors Classifier": KNeighborsClassifier(),
-                "XGBRegressor": XGBClassifier(), 
+                "XGBClassifier": XGBClassifier(), 
                 "CatBoosting Classifier": CatBoostClassifier(verbose=False),
+            }
+
+            hyperparameters = {
+                "Logistic Regression": {
+                    'C': [0.01, 0.1, 1, 10, 100],
+                    'solver': ['liblinear', 'saga'],
+                    # 'penalty': ['l1', 'l2'],
+                    # 'max_iter': [100, 500, 1000]
+                },
+                
+                "Decision Tree": {
+                    'max_depth': [None, 5, 10, 20, 50],
+                    'min_samples_split': [2, 5, 10],
+                    # 'min_samples_leaf': [1, 2, 5],
+                    # 'criterion': ['gini', 'entropy', 'log_loss']
+                },
+                
+                "Random Forest Classifier": {
+                    'n_estimators': [100, 200, 500],
+                    'max_depth': [None, 5, 10, 20,],
+                    # 'min_samples_split': [2, 5, 10],
+                    # 'min_samples_leaf': [1, 2, 5],
+                    # 'max_features': ['sqrt', 'log2', None],
+                    # 'bootstrap': [True, False]
+                },
+                
+                "XGBClassifier": {
+                    'n_estimators': [100, 200, 500],
+                    'max_depth': [3, 5, 10],
+                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                    # 'subsample': [0.6, 0.8, 1.0],
+                    # 'colsample_bytree': [0.6, 0.8, 1.0],
+                    # 'gamma': [0, 0.1, 0.5, 1],
+                    # 'reg_alpha': [0, 0.01, 0.1],
+                    # 'reg_lambda': [1, 1.5, 2.0]
+                },
+
+                "K-Neighbors Classifier": {
+                    'n_neighbors': [3, 5, 7, 10, 15],
+                    # 'weights': ['uniform', 'distance'],
+                    # 'metric': ['euclidean', 'manhattan', 'minkowski']
+                },
+                
+                
+                "CatBoosting Classifier": {
+                    'iterations': [100, 300, 500],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'depth': [4, 6, 10],
+                    'l2_leaf_reg': [1, 3, 5, 7],
+                    'border_count': [32, 64, 128],
+                    'bagging_temperature': [0, 0.5, 1]
+                }
             }
 
             model_report:dict = evaluate_models(
@@ -45,7 +97,8 @@ class ModelTrainer:
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models)
+                models=models,
+                params = hyperparameters)
 
 
             ## To get best model score from dict
